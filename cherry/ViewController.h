b2207854226849c4e9b835c651451eba5bc777cf
Joe Harding <joe.harding@us.ibm.com>
2018-01-14 15:39:34 -0600

    Initial Commit

---
+++ cherry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cherry
+//
+//  Created by Joseph Harding on 1/14/18.
+//  Copyright © 2018 Joseph Harding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cherry/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  cherry
+//
+//  Created by Joseph Harding on 1/14/18.
+//  Copyright © 2018 Joseph Harding. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
